b7621c569ad3afa295527f93c7c51ce3f0a81c32
Rik Claessens <rhmclaessens@gmail.com>
2023-12-01 13:58:04 +0100

    better parseDigit

--- src/day01/solve.ts
+++ src/day01/solve.ts
@@ -17,30 +17,18 @@ export const part1 = (input: string[]): number => {
   return sum;
 };
 
-const parseDigit = (digit: string): number => {
-  switch (digit) {
-    case 'one':
-      return 1;
-    case 'two':
-      return 2;
-    case 'three':
-      return 3;
-    case 'four':
-      return 4;
-    case 'five':
-      return 5;
-    case 'six':
-      return 6;
-    case 'seven':
-      return 7;
-    case 'eight':
-      return 8;
-    case 'nine':
-      return 9;
-    default:
-      return Number.parseInt(digit);
-  }
-};
+const parseDigit = (digit: string): string =>
+  digit
+    .replace('one', '1')
+    .replace('two', '2')
+    .replace('three', '3')
+    .replace('four', '4')
+    .replace('five', '5')
+    .replace('six', '6')
+    .replace('seven', '7')
+    .replace('eight', '8')
+    .replace('nine', '9');
+
 export const part2 = (input: string[]): number => {
   let sum = 0;
   input.forEach((line) => {
