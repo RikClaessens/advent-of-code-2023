046c2385b06b322183ac689f91acce4f46b32ea5
Rik Claessens <rhmclaessens@gmail.com>
2024-12-29 12:20:03 +0100

    day18

--- src/day18/solve.test.ts
+++ src/day18/solve.test.ts
@@ -3,9 +3,9 @@ import { expect, test } from 'bun:test';
 import { day, part1, part2, testInput } from './solve';
 
 test(`Day ${day} part 1`, () => {
-  // expect(part1(testInput)).toBe(62);
+  expect(part1(testInput)).toBe(62);
 });
 
 test(`Day ${day} part 2`, () => {
-  expect(part2(testInput)).toBe(0);
+  expect(part2(testInput)).toBe(952408144115);
 });
--- src/day18/solve.ts
+++ src/day18/solve.ts
@@ -1,116 +1,60 @@
 import { getInput } from '../getInput';
-import fs from 'fs';
 
 export const day = 'day18';
 export const testInput = getInput(`src/${day}/test.txt`);
 const input = getInput(`src/${day}/input.txt`);
 
-const dirs = [
-  [0, -1],
-  [1, 0],
-  [0, 1],
-  [-1, 0],
-];
-export const part1 = (input: string[]): number => {
-  const allCorners: [number, number][] = [];
-  allCorners.push([0, 0]);
-  let maxX = 0,
-    minX = 0,
-    maxY = 0,
-    minY = 0;
-  input.forEach((step) => {
-    const [x, y] = allCorners[allCorners.length - 1];
-    const [dir, dist] = [step[0], parseInt(step.split(' ')[1])];
+type GetDirDist = (step: string) => [string, number];
 
+const getArea = (input: string[], getDirDist: GetDirDist): number => {
+  let x = 0;
+  let y = 0;
+
+  let prevX = 0;
+  let prevY = 0;
+
+  let area = 0;
+
+  input.forEach((step) => {
+    const [dir, dist] = getDirDist(step);
+    x = prevX;
+    y = prevY;
     switch (dir) {
       case 'U':
-        allCorners.push([x, y - dist]);
-        break;
-      case 'R':
-        allCorners.push([x + dist, y]);
+        x = prevX - dist;
         break;
       case 'D':
-        allCorners.push([x, y + dist]);
+        x = prevX + dist;
+        break;
+      case 'R':
+        y = prevY + dist;
         break;
       case 'L':
-        allCorners.push([x - dist, y]);
+        y = prevY - dist;
         break;
       default:
         break;
     }
+
+    area += prevY * x - prevX * y + dist;
+    prevX = x;
+    prevY = y;
   });
-  allCorners.forEach((corner) => {
-    maxX = Math.max(maxX, corner[0]);
-    minX = Math.min(minX, corner[0]);
-    maxY = Math.max(maxY, corner[1]);
-    minY = Math.min(minY, corner[1]);
-  });
+  return area / 2 + 1;
+};
 
-  const h = maxY - minY + 1;
-  const w = maxX - minX + 1;
-
-  const grid = Array.from({ length: h }, () =>
-    Array.from({ length: w }, () => '.'),
-  );
-
-  allCorners.forEach((corner) => {
-    corner[0] -= minX;
-    corner[1] -= minY;
-  });
-
-  allCorners.forEach((corner, cornerIndex) => {
-    grid[corner[1]][corner[0]] = '#';
-    if (cornerIndex > 0) {
-      const [x1, y1] = allCorners[cornerIndex - 1];
-      const [x2, y2] = allCorners[cornerIndex];
-      if (x1 === x2) {
-        for (let y = Math.min(y1, y2); y <= Math.max(y1, y2); y++) {
-          grid[y][x1] = '#';
-        }
-      } else {
-        for (let x = Math.min(x1, x2); x <= Math.max(x1, x2); x++) {
-          grid[y1][x] = '#';
-        }
-      }
-    }
-  });
-
-  // fill all spaces in the grid that are within the wall of #'s
-
-  let startFillX = allCorners[0][0],
-    startFillY = allCorners[0][1];
-  const firstTwoSteps = [input[0][0], input[1][0]];
-  startFillY += firstTwoSteps.includes('U') ? 1 : -1;
-  startFillX += firstTwoSteps.includes('L') ? 1 : -1;
-
-  const queue: [number, number][] = [[startFillX, startFillY]];
-
-  while (queue.length) {
-    const [x, y] = queue.shift()!;
-    if (grid[y][x] === '.') {
-      grid[y][x] = '#';
-      dirs.forEach(([dx, dy]) => {
-        if (x + dx >= 0 && x + dx < w && y + dy >= 0 && y + dy < h) {
-          queue.push([x + dx, y + dy]);
-        }
-      });
-    }
-  }
-
-  let count = grid.reduce((acc, row) => {
-    return (acc += row.filter((cell) => cell === '#').length);
-  }, 0);
-
-  return count;
+export const part1 = (input: string[]): number => {
+  return getArea(input, (step) => [step[0], parseInt(step.split(' ')[1])]);
 };
 
 export const part2 = (input: string[]): number => {
-  return 0;
+  return getArea(input, (step) => [
+    'RDLU'[parseInt(step.slice(-2, -1))],
+    parseInt(step.slice(-7, -2), 16),
+  ]);
 };
 
 (() => {
   console.log('Part 1: ' + part1(input));
   console.log('Part 2: ' + part2(input));
 })();
-
-// 44543 too high
